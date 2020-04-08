a2c17088bd4fcabcf2a4f1a219a99fefa5b45e53
albinpk <abnpk7@gmail.com>
2020-04-08 23:42:13 +0530

    third commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ export default function simpleBorder(opt) {
   const el = document.querySelectorAll(".add-border");
 
   let addStyle = `border-color: ${
-    opt ? (opt.color ? opt.color : "#d3d3d3") : "black"
+    opt ? (opt.color ? opt.color : "#d3d3d3") : "#d3d3d3"
   }; border-width: ${
     opt ? (opt.width ? opt.width : "1px") : "1px"
   }; border-radius: ${
-    opt ? (opt.radius ? opt.radius : "unset") : "1px"
+    opt ? (opt.radius ? opt.radius : "unset") : "unset"
   }; border-style: solid`;
 
   el.forEach((e) => (e.style = addStyle));
